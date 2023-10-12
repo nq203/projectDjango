52781c1143e65f9aa2cb08c29b5b2bd593c56237
nq203 <quanchun8@gmail.com>
2023-10-12 22:01:04 +0700

    committ

--- store/models/customer.py
+++ store/models/customer.py
@@ -18,8 +18,6 @@ class Customer(models.Model):
             return Customer.objects.get(email= email)
         except:
             return False
-
-
     def isExists(self):
         if Customer.objects.filter(email = self.email):
             return True
